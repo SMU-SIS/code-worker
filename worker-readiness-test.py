e3fc98e9a862fe37aa336470880b6fd3443c9c43
Chris Boesch <scboesch@Chris-Boeschs-MacBook-Pro-2.local>
2011-07-05 11:00:33 +0800

    Added woker-readiness-test.py to clear TEST jobs and create new TEST jobs for the latest worker code to process.

---
+++ worker-readiness-test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib2, json
+
+baseurl = 'http://code-comparison.appspot.com/rest/'                        
+
+def get_unprocessed_test_jobs():
+  url = baseurl+'Job?feq_jobType=TEST&fne_status=PROCESSED'                   
+  data = None                                                                 
+  result = json.loads(urllib2.urlopen(urllib2.Request(url, data, {'Content-Type': 'application/json'})).read())
+  print 'There are currently', len(result), 'unprocessed TEST jobs.'
+  return result
+
+#You can filter results on the REST interface using feq_ and fne_. 
+#This means that we can make sure that there are no jobs of jobType=TEST& status!=PROCESSED. If there are, we can mark all the TEST jobs as PROCESSED.  
+
+result = get_unprocessed_test_jobs()
+
+for k in result:
+  data = json.dumps({'status':'PROCESSED'})
+  url = baseurl + 'Job/'+k['key']
+  result = json.loads(urllib2.urlopen(urllib2.Request(url, data, {'Content-Type': 'application/json'})).read())
+  print 'Marking existing TEST job as PROCESSED'
+  
+#Now that we now that there are no TEST jobs that have not been marked as PROCESSED, we can create some new TEST jobs for the worker to process. 
+result = get_unprocessed_test_jobs()
+
+for command in ['python -V', 'java -version', 'git --version']:
+  url = baseurl+'Job'
+  data = json.dumps({'command':command, 'jobType':'TEST'})
+  print 'Creating job to run command', command, url, data                  
+  result = json.loads(urllib2.urlopen(urllib2.Request(url, data, {'Content-Type': 'application/json'})).read())
+
+result = get_unprocessed_test_jobs()
+
+# Now check back every 10 seconds to see if the TEST jobs have been processed by the worker. If they don't all get processed in a minute or two, the integration test would fail.   
+# The next time this script is run, all these TEST jobs well be marked as PROCESSED
